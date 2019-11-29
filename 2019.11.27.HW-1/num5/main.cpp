e2e90226ff780ace5efad5a4990a3338fd717b96
alexdtat <57017816+alexdtat@users.noreply.github.com>
2019-11-29 23:25:44 +0300

    Задача_5
    Исправлена задача 5

--- 2019.11.27.HW-1/num5/main.cpp
+++ 2019.11.27.HW-1/num5/main.cpp
@@ -15,35 +15,31 @@ bool is_prime_number (int num){
 int main()
 {
     int n = 0;
-    int counter = 0;
     int p = 2;
     cin >> n;
-    short int* nat_numbers = new short int [n*n+1];
-    for (int k = 0; k < n*n; k++){
+    short int* nat_numbers = new short int [n+2];
+    for (int k = 0; k < n + 2; k++){
         nat_numbers[k] = 0;
     };
     nat_numbers[0] = 1;
     nat_numbers[1] = 1;
-    for (int i = 2; i < n*n; i++){
+    for (int i = 2; i < n + 2; i++){
         if (nat_numbers[i] == 0){
             if (is_prime_number(i)){
-                for (int j = 2; i*j < n*n; j++){
-                    if ((nat_numbers[j] == 0) && (j <= i)) nat_numbers[i*j] = -1;
-                    else nat_numbers [i*j] = 1;
+                for (int j = 2; i*j < n+2; j++){
+                    nat_numbers[i*j] = j;
                 }
             }
         }
     };
 
-    while (counter <= n){
-        if ((nat_numbers[p] == -1) && (nat_numbers[p-2] == 0)){
+    while (p <= n + 2){
+        if ((nat_numbers[p] != 0) && (nat_numbers[p-2] == 0) && (nat_numbers[p/nat_numbers[p]] == 0)){
             cout << p - 2 << ' ';
-            counter++;
         };
 
-        if ((nat_numbers[p] == 0) && (nat_numbers[p-2] == 0) && (n-counter >= 0)){
+        if ((nat_numbers[p] == 0) && (nat_numbers[p-2] == 0)){
             cout << p - 2 << ' ';
-            counter++;
         };
         p++;
     }
